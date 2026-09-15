891b6c6e6de7857de9eb6570e32a8123ac14593d
GlaugTbest <ghostgamer20162000@gmail.com>
2026-09-15 11:18:12 -0300

    chore: keep repository focused on runtime code

--- apps/api/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/**/*.spec.ts'],
-  clearMocks: true,
-  transform: { '^.+\\.[jt]s$': ['ts-jest', { tsconfig: { allowJs: true } }] },
-  transformIgnorePatterns: ['node_modules/(?!@nestjs/(bullmq|bull-shared)/)'],
-};
